f6ccd87d59cb026e6b006f7587b1046b23047626
omar11011 <atarama11011@gmail.com>
2020-07-22 02:04:47 -0500

    Config express

--- bot.js
+++ bot.js
@@ -37,6 +37,10 @@ client.login(process.env.TOKEN)
 
 // Express
 
-const app = express()
-
-app.listen(3000, () => console.log("Express listo!"))
+var app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
