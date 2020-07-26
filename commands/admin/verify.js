ef34fc8980be7d62953f3b942577e8590ed2ab01
Feli <srgatoiscool@gmail.com>
2020-07-26 00:31:09 -0500

    change emoji for env

--- commands/admin/verify.js
+++ commands/admin/verify.js
@@ -11,7 +11,7 @@ module.exports = {
       .setTitle("Pick your roles!")
       .setDescription(`1️⃣ Omar \n`) // We're gonna try an unicode emoji. Let's find it on emojipedia.com !
     message.channel.send(embed).then(async msg => {
-      await msg.react("1️⃣");
+      await msg.react(process.env.EMOJI_REACT);
       // We're gonna using an await, to make the react are right in order.
     });
   }
