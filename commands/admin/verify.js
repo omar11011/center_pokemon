48873abe0d1f085a2b37498a268e62273a830cc1
Feli <srgatoiscool@gmail.com>
2020-08-06 21:39:31 -0500

    :sparkles: add more information

--- commands/admin/verify.js
+++ commands/admin/verify.js
@@ -7,9 +7,16 @@ module.exports = {
   guildOnly: true,
   async execute(message, args) {
     const embed = new Discord.MessageEmbed()
-      .setColor(0xffffff)
-      .setTitle("Pick your roles!")
-      .setDescription(`1️⃣ Omar \n`) // We're gonna try an unicode emoji. Let's find it on emojipedia.com !
+      .setColor('RANDOM')
+      .setTitle("Hey!")
+      .setImage('https://media.tenor.com/images/49e62f2a4fc1075636d89e381aa30486/tenor.gif')
+      .setDescription(`Welcome to the  Official Server  of  <@682622549104001063>. Only those trainers who have already started their journey through a region may enter.
+
+      <#735375680400916581>: Use this channel if you have not yet registered as a Pokémon trainer. Only the s!start and s!region commands are supported
+      
+      When you react you will be able to see the other channels, and also I will send you a message by md where I will explain each thing better.
+      
+      Remember, I won't be able to talk to you if you have the direct messages disabled, so please keep this in mind.\n`) // We're gonna try an unicode emoji. Let's find it on emojipedia.com !
     message.channel.send(embed).then(async msg => {
       await msg.react(process.env.EMOJI_REACT);
       // We're gonna using an await, to make the react are right in order.
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,9 +3,13 @@ const Discord = require('discord.js')
 
 module.exports = async (reaction, object, user) => {
   const embed = new Discord.MessageEmbed()
-    .setColor(0xffffff)
-    .setTitle("Welcome!")
-    .setDescription(`Simple test \n`)
+    .setColor('RANDOM')
+    .setTitle("Welcome! <a:pokesaludo:697872213201780808>")
+    .setDescription(`Just like in the games, catching pokemons is not as simple as saying their name.
+
+    You will have a rate that will calculate if you will catch the pokemon or not, remember use **s!bonus**, we will launch more information on the official site!
+    
+    With nothing more to say we hope you have a good time, and have a nice travel!\n`)
 
   const trainer = await Trainers.findOne({
     where: {
@@ -24,8 +28,6 @@ module.exports = async (reaction, object, user) => {
   // get the role
   let role = await object.message.guild.roles.cache.find(r => r.name === trainer.Region)
   let roletrainer = await object.message.guild.roles.cache.find(r => r.name === 'Trainer')
-  await object.message.guild.members.cache.get(user.id).roles.add(roletrainer.id) // Minecraft role.
-
 
   if (object.message.partial) await object.message.fetch(); // Partial messages do not contain any content so skip them.
   if (object.partial) await object.fetch();
@@ -34,13 +36,15 @@ module.exports = async (reaction, object, user) => {
   if (!object.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
   if (object.message.guild.id !== process.env.SERVER_ID) return; // Use this if your bot was only for one server/private server.
 
-  if (object.message.channel.id === process.env.CHANNEL_ID) { // This is a #self-roles channel.
-    if (object.emoji.name === process.env.EMOJI_REACT) {
-      await object.message.guild.members.cache.get(user.id).roles.add(role.id) // Minecraft role.
-      return user.send(embed).catch(() => console.log("Failed to send DM."));
-    }
+  await object.message.guild.members.cache.get(user.id).roles.add(roletrainer.id) // Minecraft role.
+  await object.message.guild.members.cache.get(user.id).roles.add(role.id) // Minecraft role.
+  return user.send(embed).catch(() => console.log("Failed to send DM."));
+  // if (object.message.channel.id === process.env.CHANNEL_ID) { // This is a #self-roles channel.
+  //   if (object.emoji.name === process.env.EMOJI_REACT) {
+  //     return user.send(embed).catch(() => console.log("Failed to send DM."));
+  //   }
 
-  } else {
-    return; // If the channel was not a #self-roles, ignore them.
-  }
+  // } else {
+  //   return; // If the channel was not a #self-roles, ignore them.
+  // }
 }
